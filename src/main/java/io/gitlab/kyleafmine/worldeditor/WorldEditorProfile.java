742827ed7acc92a5d8c4bf89c580019025b57f25
Kyle Smith <kyleafmine@gmail.com>
2021-03-24 16:04:03 -0300

    god i hate git

--- src/main/java/io/gitlab/kyleafmine/worldeditor/WorldEditorProfile.java
+++ src/main/java/io/gitlab/kyleafmine/worldeditor/WorldEditorProfile.java
@@ -4,13 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WorldEditorProfile extends JavaPlugin {
-
+    public static WorldEditorProfile instance;
     @Override
     public void onEnable() {
-/*
-        Bukkit.getOfflinePlayerIfCached("Courtsey_Call").banPlayer("Annoying");
-        Bukkit.getOfflinePlayerIfCached("jackifer").banPlayer("Name starts with j");
-        Bukkit.getOfflinePlayerIfCached("carlmarcotte").banPlayer("dropping ib");*/
+        instance = this;
         getLogger().info("So this is a bad idea i don't know if hotloading is great");
     }
 }
